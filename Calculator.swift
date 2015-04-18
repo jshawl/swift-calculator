d90d42f59baa55acc990c5e88d9d0405d7171ede
Jesse Shawl <jesse@jshawl.com>
2015-04-18 15:08:43 -0400

    wip - objectifyin

---
+++ Calculator.swift
@@ -0,0 +1,35 @@
+//
+//  Calculator.swift
+//  swift-calculator
+//
+//  Created by Jesse Shawl on 4/18/15.
+//  Copyright (c) 2015 Jesse Shawl. All rights reserved.
+//
+
+import Foundation
+
+
+
+class Calculator {
+    
+    init(){
+        
+    }
+    
+    var n:Int?
+    var n2:Int?
+    var operand:String?
+    
+    func eval( n: Int, operand:String, n2:Int ) -> Int {
+        if operand == "+"{
+            println("operand was plus")
+            return n + n2
+        }
+        return n
+    }
+    func add(n:Int, n2:Int) -> Int{
+        return n + n2
+    }
+    
+}
+
--- swift-calculator/ViewController.swift
+++ swift-calculator/ViewController.swift
@@ -9,27 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-
+    var calculator = Calculator()
     @IBOutlet weak var numberDisplay: UILabel!
+    @IBAction func operand(sender: AnyObject) {
+        calculator.operand = sender.currentTitle!!
+        numberDisplay?.text = numberDisplay!.text! + calculator.operand!
+        if calculator.n? != nil {
+            if calculator.n2? != nil {
+                let result = calculator.eval( calculator.n!, operand:calculator.operand!, n2: calculator.n2! )
+                numberDisplay?.text = "\(result)"
+                calculator = Calculator()
+            }
+        }
+    }
     @IBAction func clear(sender: AnyObject) {
         numberDisplay?.text = ""
     }
     @IBAction func numberPress(sender: AnyObject) {
+        var n = calculator.n ?? 0
         if let number = sender.currentTitle!{
             numberDisplay?.text = numberDisplay!.text! + number
+            if calculator.operand == nil {
+                calculator.n = n + number.toInt()!
+            } else {
+                calculator.n2 = n + number.toInt()!
+            }
         }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
